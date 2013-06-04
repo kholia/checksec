1674c58cbf83e13b2ac4d8e9302a546f3c231f54
Dhiru Kholia <dhiru@openwall.com>
2013-06-04 13:13:20 +0530

    start tracking saucy

--- fetch_ubuntu_repodata.sh
+++ fetch_ubuntu_repodata.sh
@@ -1,4 +1,4 @@
 for item in main multiverse restricted universe
 do
-rsync --progress -avrz --exclude='*.bz2' rsync://archive.ubuntu.com/ubuntu/dists/raring/$item/binary-amd64/ data/ubuntu/$item
+rsync --progress -avrz --exclude='*.bz2' rsync://archive.ubuntu.com/ubuntu/dists/saucy/$item/binary-amd64/ data/$item
 done
